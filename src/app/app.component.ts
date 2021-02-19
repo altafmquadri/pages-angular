73cafeb89b1a0fc520034d12024470e2c9c91a3a
Altaf Quadri <altafmquadri@gmail.com>
2021-02-19 17:18:26 -0500

    examples of ng-container with combined for and if, ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  currentPage = 3;
+  currentPage = 0;
 
   images = [
     {
@@ -29,5 +29,89 @@ export class AppComponent {
       url:
         'https://images.unsplash.com/photo-1468577760773-139c2f1c335f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8amFtfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
     },
+    {
+      title: 'At the Beach',
+      url:
+        'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhY2h8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
+    {
+      title: 'Oh Sunny Day',
+      url:
+        'https://images.unsplash.com/photo-1563630381190-77c336ea545a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c3VufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
+    {
+      title: 'A white Rose',
+      url:
+        'https://images.unsplash.com/photo-1495231916356-a86217efff12?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Zmxvd2VyfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
+    {
+      title: 'The perfect snack',
+      url:
+        'https://images.unsplash.com/photo-1468577760773-139c2f1c335f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8amFtfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url:
+        'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhY2h8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
+    {
+      title: 'Oh Sunny Day',
+      url:
+        'https://images.unsplash.com/photo-1563630381190-77c336ea545a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c3VufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
+    {
+      title: 'A white Rose',
+      url:
+        'https://images.unsplash.com/photo-1495231916356-a86217efff12?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Zmxvd2VyfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
+    {
+      title: 'The perfect snack',
+      url:
+        'https://images.unsplash.com/photo-1468577760773-139c2f1c335f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8amFtfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url:
+        'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhY2h8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
+    {
+      title: 'Oh Sunny Day',
+      url:
+        'https://images.unsplash.com/photo-1563630381190-77c336ea545a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c3VufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
+    {
+      title: 'A white Rose',
+      url:
+        'https://images.unsplash.com/photo-1495231916356-a86217efff12?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Zmxvd2VyfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
+    {
+      title: 'The perfect snack',
+      url:
+        'https://images.unsplash.com/photo-1468577760773-139c2f1c335f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8amFtfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
+    {
+      title: 'At the Beach',
+      url:
+        'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhY2h8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
+    {
+      title: 'Oh Sunny Day',
+      url:
+        'https://images.unsplash.com/photo-1563630381190-77c336ea545a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c3VufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
+    {
+      title: 'A white Rose',
+      url:
+        'https://images.unsplash.com/photo-1495231916356-a86217efff12?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8Zmxvd2VyfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
+    {
+      title: 'The perfect snack',
+      url:
+        'https://images.unsplash.com/photo-1468577760773-139c2f1c335f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8amFtfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+    },
   ];
+
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5
+  }
 }
